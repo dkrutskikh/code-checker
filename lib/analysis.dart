e29bd05e1fc3c72e8c36077dc640a2b3bd45fb9b
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2020-12-28 13:54:01 +0300

    add ProcessedFile

--- lib/analysis.dart
+++ lib/analysis.dart
@@ -1,1 +1,2 @@
 export 'package:code_checker/src/models/severity.dart';
+export 'package:code_checker/src/models/processed_file.dart';
---
+++ lib/src/models/processed_file.dart
@@ -0,0 +1,15 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:meta/meta.dart';
+
+/// Represents a file that is being processed
+@immutable
+class ProcessedFile {
+  /// Path of the file that is being processed
+  final Uri url;
+  /// Representation content of the file in plain text
+  final String content;
+  /// Representation content of the file in AST structure
+  final CompilationUnit parsedContent;
+
+  const ProcessedFile(this.url, this.content, this.parsedContent);
+}

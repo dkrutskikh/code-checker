1fb8cc4d7eec6f882399ea3f11852f0d952986b6
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2020-12-29 11:20:45 +0300

    issue

--- lib/analysis.dart
+++ lib/analysis.dart
@@ -1,2 +1,3 @@
-export 'package:code_checker/src/models/severity.dart';
+export 'package:code_checker/src/models/issue.dart';
 export 'package:code_checker/src/models/processed_file.dart';
+export 'package:code_checker/src/models/severity.dart';

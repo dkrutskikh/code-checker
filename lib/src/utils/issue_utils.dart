8ab1ab090e9fd9ce0f2c7a8da1e1fb5df0bcead6
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2020-12-30 15:35:24 +0300

    add util function createIssue (#8)
    * add util function createIssue
    
    * fix prefer-tailing-comma issue

---
+++ lib/src/utils/issue_utils.dart
@@ -0,0 +1,20 @@
+import 'package:source_span/source_span.dart';
+
+import '../models/issue.dart';
+import '../models/replacement.dart';
+import '../rules/rule.dart';
+
+Issue createIssue(
+  Rule rule,
+  SourceSpanBase location,
+  String message,
+  Replacement replacement,
+) =>
+    Issue(
+      ruleId: rule.id,
+      documentation: rule.documentation,
+      location: location,
+      severity: rule.severity,
+      message: message,
+      suggestion: replacement,
+    );

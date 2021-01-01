bf0cc919680e86a53a78dfefb85b5de76a017628
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-01 11:47:12 +0300

    add ScopedFunctionDeclaration

--- lib/checker.dart
+++ lib/checker.dart
@@ -1,2 +1,3 @@
 export 'package:code_checker/src/models/function_type.dart';
 export 'package:code_checker/src/models/scoped_class_declaration.dart';
+export 'package:code_checker/src/models/scoped_function_declaration.dart';
---
+++ lib/src/models/scoped_function_declaration.dart
@@ -0,0 +1,18 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:meta/meta.dart';
+
+import 'function_type.dart';
+import 'scoped_class_declaration.dart';
+
+@immutable
+class ScopedFunctionDeclaration {
+  final FunctionType type;
+  final Declaration declaration;
+  final ScopedClassDeclaration enclosingDeclaration;
+
+  const ScopedFunctionDeclaration(
+    this.type,
+    this.declaration,
+    this.enclosingDeclaration,
+  );
+}

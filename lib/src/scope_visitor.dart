354f7943abf8209ee16b3e45e102b4d22a00e1bb
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-02 17:52:39 +0300

    add ScopeVisitor

---
+++ lib/src/scope_visitor.dart
@@ -0,0 +1,81 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+
+import 'models/function_type.dart';
+import 'models/scoped_class_declaration.dart';
+import 'models/scoped_function_declaration.dart';
+
+/// Visitor to collect declarations of classes and functions
+class ScopeVisitor extends RecursiveAstVisitor<void> {
+  final _components = <ScopedClassDeclaration>[];
+  final _functions = <ScopedFunctionDeclaration>[];
+
+  ScopedClassDeclaration _enclosingDeclaration;
+
+  Iterable<ScopedClassDeclaration> get components => _components;
+
+  Iterable<ScopedFunctionDeclaration> get functions => _functions;
+
+  @override
+  void visitClassDeclaration(ClassDeclaration node) {
+    _registerClassDeclaration(node, () {
+      super.visitClassDeclaration(node);
+    });
+  }
+
+  @override
+  void visitConstructorDeclaration(ConstructorDeclaration node) {
+    if (node.body is! EmptyFunctionBody) {
+      _registerFunctionDeclaration(FunctionType.constructor, node);
+    }
+
+    super.visitConstructorDeclaration(node);
+  }
+
+  @override
+  void visitExtensionDeclaration(ExtensionDeclaration node) {
+    _registerClassDeclaration(node, () {
+      super.visitExtensionDeclaration(node);
+    });
+  }
+
+  @override
+  void visitFunctionDeclaration(FunctionDeclaration node) {
+    _registerFunctionDeclaration(FunctionType.function, node);
+
+    super.visitFunctionDeclaration(node);
+  }
+
+  @override
+  void visitMethodDeclaration(MethodDeclaration node) {
+    if (node.body is! EmptyFunctionBody) {
+      _registerFunctionDeclaration(FunctionType.method, node);
+    }
+
+    super.visitMethodDeclaration(node);
+  }
+
+  @override
+  void visitMixinDeclaration(MixinDeclaration node) {
+    _registerClassDeclaration(node, () {
+      super.visitMixinDeclaration(node);
+    });
+  }
+
+  void _registerClassDeclaration(
+    CompilationUnitMember node,
+    void Function() visitCallback,
+  ) {
+    _enclosingDeclaration = ScopedClassDeclaration(node);
+    _components.add(_enclosingDeclaration);
+
+    visitCallback();
+
+    _enclosingDeclaration = null;
+  }
+
+  void _registerFunctionDeclaration(FunctionType type, Declaration node) {
+    _functions
+        .add(ScopedFunctionDeclaration(type, node, _enclosingDeclaration));
+  }
+}
---
+++ test/scope_visitor_test.dart
@@ -0,0 +1,207 @@
+@TestOn('vm')
+import 'package:analyzer/dart/analysis/features.dart';
+import 'package:analyzer/dart/analysis/utilities.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:code_checker/checker.dart';
+import 'package:code_checker/src/scope_visitor.dart';
+import 'package:path/path.dart' as p;
+import 'package:test/test.dart';
+
+void main() {
+  group('ScopeVisitor collects scope from file with', () {
+    ScopeVisitor visitor;
+
+    setUp(() {
+      visitor = ScopeVisitor();
+    });
+
+    test('abstract class', () {
+      parseFile(
+        path: p.normalize(p.absolute('./test/resources/abstract_class.dart')),
+        featureSet: FeatureSet.fromEnableFlags([]),
+      ).unit.visitChildren(visitor);
+
+      final component = visitor.components.single;
+      expect(component.declaration, const TypeMatcher<ClassDeclaration>());
+      expect(
+        (component.declaration as ClassDeclaration).name.name,
+        equals('Doer'),
+      );
+
+      expect(visitor.functions, isEmpty);
+    });
+
+    test('class with factory constructors', () {
+      parseFile(
+        path: p.normalize(p
+            .absolute('./test/resources/class_with_factory_constructors.dart')),
+        featureSet: FeatureSet.fromEnableFlags([]),
+      ).unit.visitChildren(visitor);
+
+      final component = visitor.components.single;
+      expect(component.declaration, const TypeMatcher<ClassDeclaration>());
+      expect(
+        (component.declaration as ClassDeclaration).name.name,
+        equals('Logger'),
+      );
+
+      final functions = visitor.functions.toList();
+      expect(functions.length, equals(3));
+
+      final constructor = functions.first;
+      expect(constructor.type, equals(FunctionType.constructor));
+      expect(
+        constructor.declaration,
+        const TypeMatcher<ConstructorDeclaration>(),
+      );
+      expect(
+        (constructor.declaration as ConstructorDeclaration).name,
+        isNull,
+      );
+      expect(
+        constructor.enclosingDeclaration.declaration,
+        const TypeMatcher<ClassDeclaration>(),
+      );
+      expect(
+        (constructor.enclosingDeclaration.declaration as ClassDeclaration)
+            .name
+            .name,
+        equals('Logger'),
+      );
+
+      final fromJson = functions[1];
+      expect(fromJson.type, equals(FunctionType.constructor));
+      expect(
+        fromJson.declaration,
+        const TypeMatcher<ConstructorDeclaration>(),
+      );
+      expect(
+        (fromJson.declaration as ConstructorDeclaration).name.name,
+        'fromJson',
+      );
+      expect(
+        fromJson.enclosingDeclaration.declaration,
+        const TypeMatcher<ClassDeclaration>(),
+      );
+      expect(
+        (fromJson.enclosingDeclaration.declaration as ClassDeclaration)
+            .name
+            .name,
+        equals('Logger'),
+      );
+
+      expect(functions.last.type, equals(FunctionType.method));
+      expect(
+        functions.last.declaration,
+        const TypeMatcher<MethodDeclaration>(),
+      );
+      expect(
+        (functions.last.declaration as MethodDeclaration).name.name,
+        equals('log'),
+      );
+      expect(
+        functions.last.enclosingDeclaration.declaration,
+        const TypeMatcher<ClassDeclaration>(),
+      );
+      expect(
+        (functions.last.enclosingDeclaration.declaration as ClassDeclaration)
+            .name
+            .name,
+        equals('Logger'),
+      );
+    });
+
+    test('extension with method', () {
+      parseFile(
+        path: p.normalize(p.absolute('./test/resources/extension.dart')),
+        featureSet: FeatureSet.fromEnableFlags([]),
+      ).unit.visitChildren(visitor);
+
+      final component = visitor.components.single;
+      expect(component.declaration, const TypeMatcher<ExtensionDeclaration>());
+      expect(
+        (component.declaration as ExtensionDeclaration).name.name,
+        equals('NumberParsing'),
+      );
+
+      final function = visitor.functions.single;
+      expect(function.type, equals(FunctionType.method));
+      expect(function.declaration, const TypeMatcher<MethodDeclaration>());
+      expect(
+        (function.declaration as MethodDeclaration).name.name,
+        equals('parseInt'),
+      );
+      expect(
+        function.enclosingDeclaration.declaration,
+        const TypeMatcher<ExtensionDeclaration>(),
+      );
+      expect(
+        (function.enclosingDeclaration.declaration as ExtensionDeclaration)
+            .name
+            .name,
+        equals('NumberParsing'),
+      );
+    });
+
+    test('mixin', () {
+      parseFile(
+        path: p.normalize(p.absolute('./test/resources/mixin.dart')),
+        featureSet: FeatureSet.fromEnableFlags([]),
+      ).unit.visitChildren(visitor);
+
+      final component = visitor.components.single;
+      expect(component.declaration, const TypeMatcher<MixinDeclaration>());
+      expect(
+        (component.declaration as MixinDeclaration).name.name,
+        equals('Musical'),
+      );
+
+      final function = visitor.functions.single;
+      expect(function.type, equals(FunctionType.method));
+      expect(function.declaration, const TypeMatcher<MethodDeclaration>());
+      expect(
+        (function.declaration as MethodDeclaration).name.name,
+        equals('entertainMe'),
+      );
+      expect(
+        function.enclosingDeclaration.declaration,
+        const TypeMatcher<MixinDeclaration>(),
+      );
+      expect(
+        (function.enclosingDeclaration.declaration as MixinDeclaration)
+            .name
+            .name,
+        equals('Musical'),
+      );
+    });
+
+    test('functions', () {
+      parseFile(
+        path: p.normalize(p.absolute('./test/resources/functions.dart')),
+        featureSet: FeatureSet.fromEnableFlags([]),
+      ).unit.visitChildren(visitor);
+
+      expect(visitor.components, isEmpty);
+
+      expect(visitor.functions.length, equals(2));
+
+      final first = visitor.functions.first;
+      expect(first.type, equals(FunctionType.function));
+      expect(first.declaration, const TypeMatcher<FunctionDeclaration>());
+      expect(
+        (first.declaration as FunctionDeclaration).name.name,
+        equals('printInteger'),
+      );
+      expect(first.enclosingDeclaration, isNull);
+
+      final last = visitor.functions.last;
+      expect(last.type, equals(FunctionType.function));
+      expect(last.declaration, const TypeMatcher<FunctionDeclaration>());
+      expect(
+        (last.declaration as FunctionDeclaration).name.name,
+        equals('main'),
+      );
+      expect(last.enclosingDeclaration, isNull);
+    });
+  });
+}

fd7b580dba4e4be01bd388f01373f054cc640b62
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-09 18:53:14 +0300

    add yaml utils

---
+++ lib/src/utils/yaml_utils.dart
@@ -0,0 +1,29 @@
+import 'package:yaml/yaml.dart';
+
+List<Object> yamlListToDartList(YamlList map) =>
+    List.unmodifiable(map.nodes.map<Object>(yamlNodeToDartObject));
+
+Map<String, Object> yamlMapToDartMap(YamlMap map) =>
+    Map.unmodifiable(Map<String, Object>.fromEntries(map.nodes.keys
+        .whereType<YamlScalar>()
+        .where((key) => key.value is String)
+        .map((key) => MapEntry(
+              key.value as String,
+              yamlNodeToDartObject(map.nodes[key]),
+            ))));
+
+Object yamlNodeToDartObject(YamlNode node) {
+  var object = Object();
+
+  if (node is YamlMap) {
+    object = yamlMapToDartMap(node);
+  } else if (node is YamlList) {
+    object = yamlListToDartList(node);
+  } else if (node is YamlScalar) {
+    object = yamlScalarToDartObject(node);
+  }
+
+  return object;
+}
+
+Object yamlScalarToDartObject(YamlScalar scalar) => scalar.value as Object;

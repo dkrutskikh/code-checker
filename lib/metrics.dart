89ec545ccd3a9c42348e04ce250c5ad5c854b93c
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-10 09:23:02 +0300

    add complexityByControlFlowType (#20)
    * reexport yaml utils
    
    * add complexityByControlFlowType

--- lib/metrics.dart
+++ lib/metrics.dart
@@ -1,3 +1,4 @@
+export 'package:code_checker/src/metrics/cyclomatic_complexity/cyclomatic_config.dart';
 export 'package:code_checker/src/metrics/metric.dart';
 export 'package:code_checker/src/metrics/number_of_methods.dart';
 export 'package:code_checker/src/models/metric_value.dart';

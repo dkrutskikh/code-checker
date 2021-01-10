cd58b335c93f2612d68822e7b4b2a9f667e40253
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-10 21:17:14 +0300

    add cli validation

--- bin/checker.dart
+++ bin/checker.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:code_checker/src/cli/arguments_parser.dart';
+import 'package:code_checker/src/cli/arguments_validation.dart';
+import 'package:code_checker/src/cli/arguments_validation_exceptions.dart';
 
 final _parser = argumentsParser();
 
@@ -11,7 +13,12 @@ Future<void> main(List<String> args) async {
     if (arguments[helpFlagName] as bool) {
       _showUsageAndExit(0);
     }
+
+    validateArguments(arguments);
   } on FormatException catch (e) {
+    print('${e.message}\n');
+    _showUsageAndExit(1);
+  } on InvalidArgumentException catch (e) {
     print('${e.message}\n');
     _showUsageAndExit(1);
   }
---
+++ lib/src/cli/arguments_validation_exceptions.dart
@@ -0,0 +1,8 @@
+import 'package:meta/meta.dart';
+
+@immutable
+class InvalidArgumentException implements Exception {
+  final String message;
+
+  const InvalidArgumentException(this.message);
+}

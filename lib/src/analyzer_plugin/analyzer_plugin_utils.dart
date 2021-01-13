5c3c226e2ac23ead7ba4443ffb810bf6877011fd
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-13 11:19:24 +0300

    add severityFromMetricValueLevel

---
+++ lib/src/analyzer_plugin/analyzer_plugin_utils.dart
@@ -0,0 +1,7 @@
+import 'package:analyzer_plugin/protocol/protocol_common.dart' as p;
+import '../models/metric_value_level.dart';
+
+p.AnalysisErrorSeverity severityFromMetricValueLevel(MetricValueLevel level) =>
+    level == MetricValueLevel.alarm
+        ? p.AnalysisErrorSeverity.WARNING
+        : p.AnalysisErrorSeverity.INFO;

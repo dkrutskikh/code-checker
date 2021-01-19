9ba061dccdba75f089bdfd212dab0c9117c4dbcb
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-19 22:28:09 +0300

    reexport metrics factory

--- lib/metrics.dart
+++ lib/metrics.dart
@@ -2,6 +2,7 @@ export 'package:code_checker/src/metrics/cyclomatic_complexity/cyclomatic_comple
 export 'package:code_checker/src/metrics/metric.dart';
 export 'package:code_checker/src/metrics/number_of_methods_metric.dart';
 export 'package:code_checker/src/metrics/weight_of_class_metric.dart';
+export 'package:code_checker/src/metrics_factory.dart';
 export 'package:code_checker/src/models/metric_value.dart';
 export 'package:code_checker/src/models/metric_value_level.dart';
 export 'package:code_checker/src/utils/metric_utils.dart';

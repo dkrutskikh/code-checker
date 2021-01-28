f2fdedc1e6b273e046c6fef2457030ee15d27961
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-28 22:27:54 +0300

    introduce Config.merge

--- test/config/config_test.dart
+++ test/config/config_test.dart
@@ -27,33 +27,104 @@ const _options = AnalysisOptions({
   },
 });
 
-void main() {
-  group('Config.fromAnalysisOptions constructs instance from passed', () {
-    test('empty options', () {
-      final config = Config.fromAnalysisOptions(const AnalysisOptions({}));
+const _defaults = Config(
+  excludePatterns: ['test/resources/**'],
+  excludeForMetricsPatterns: ['test/**'],
+  metrics: {
+    'metric-id1': '15',
+    'metric-id2': '10',
+    'metric-id3': '5',
+  },
+);
 
-      expect(config.excludePatterns, isEmpty);
-      expect(config.excludeForMetricsPatterns, isEmpty);
-      expect(config.metrics, isEmpty);
+const _empty = Config(
+  excludePatterns: [],
+  excludeForMetricsPatterns: [],
+  metrics: {},
+);
+
+const _merged = Config(
+  excludePatterns: ['test/resources/**'],
+  excludeForMetricsPatterns: ['test/**', 'examples/**'],
+  metrics: {
+    'metric-id1': '5',
+    'metric-id2': '10',
+    'metric-id3': '5',
+    'metric-id4': '0',
+  },
+);
+
+const _overrides = Config(
+  excludePatterns: [],
+  excludeForMetricsPatterns: ['examples/**'],
+  metrics: {
+    'metric-id1': '5',
+    'metric-id4': '0',
+  },
+);
+
+void main() {
+  group('Config', () {
+    group('fromAnalysisOptions constructs instance from passed', () {
+      test('empty options', () {
+        final config = Config.fromAnalysisOptions(const AnalysisOptions({}));
+
+        expect(config.excludePatterns, isEmpty);
+        expect(config.excludeForMetricsPatterns, isEmpty);
+        expect(config.metrics, isEmpty);
+      });
+
+      test('data', () {
+        final config = Config.fromAnalysisOptions(_options);
+
+        expect(config.excludePatterns, equals(['test/resources/**']));
+        expect(
+          config.excludeForMetricsPatterns,
+          equals(['test/**', 'examples/**']),
+        );
+        expect(
+          config.metrics,
+          equals({
+            'metric-id1': '5',
+            'metric-id2': '10',
+            'metric-id3': '5',
+            'metric-id4': '0',
+          }),
+        );
+      });
     });
 
-    test('data', () {
-      final config = Config.fromAnalysisOptions(_options);
+    group('merge constructs instance with data from', () {
+      test('defaults and empty configs', () {
+        final result = _defaults.merge(_empty);
 
-      expect(config.excludePatterns, equals(['test/resources/**']));
-      expect(
-        config.excludeForMetricsPatterns,
-        equals(['test/**', 'examples/**']),
-      );
-      expect(
-        config.metrics,
-        equals({
-          'metric-id1': '5',
-          'metric-id2': '10',
-          'metric-id3': '5',
-          'metric-id4': '0',
-        }),
-      );
+        expect(result.excludePatterns, equals(_defaults.excludePatterns));
+        expect(
+          result.excludeForMetricsPatterns,
+          equals(_defaults.excludeForMetricsPatterns),
+        );
+        expect(result.metrics, equals(_defaults.metrics));
+      });
+      test('empty and overrides configs', () {
+        final result = _empty.merge(_overrides);
+
+        expect(result.excludePatterns, equals(_overrides.excludePatterns));
+        expect(
+          result.excludeForMetricsPatterns,
+          equals(_overrides.excludeForMetricsPatterns),
+        );
+        expect(result.metrics, equals(_overrides.metrics));
+      });
+      test('defaults and overrides configs', () {
+        final result = _defaults.merge(_overrides);
+
+        expect(result.excludePatterns, equals(_merged.excludePatterns));
+        expect(
+          result.excludeForMetricsPatterns,
+          equals(_merged.excludeForMetricsPatterns),
+        );
+        expect(result.metrics, equals(_merged.metrics));
+      });
     });
   });
 }

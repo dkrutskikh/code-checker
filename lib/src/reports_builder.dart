797ea3effb8a460f40bdfd21daa598c6ad5de421
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-28 20:34:24 +0300

    recordDesignIssues renamed to recordDesignIssues

--- lib/src/reports_builder.dart
+++ lib/src/reports_builder.dart
@@ -4,7 +4,7 @@ import 'package:code_checker/rules.dart';
 abstract class ReportsBuilder {
   void recordClass(ScopedClassDeclaration declaration, ClassReport report);
 
-  void recordDesignIssues(Iterable<Issue> issues);
+  void recordAntiPatternCases(Iterable<Issue> issues);
 
   void recordIssues(Iterable<Issue> issues);
 }

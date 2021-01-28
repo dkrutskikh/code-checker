9d5cb087c3aa68f5e5b104a6324372943a9750ae
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-28 20:12:03 +0300

    introduce ReportsBuilder.recordFunction

--- lib/src/reports_builder.dart
+++ lib/src/reports_builder.dart
@@ -1,10 +1,18 @@
-import 'package:code_checker/checker.dart';
-import 'package:code_checker/rules.dart';
+import 'models/class_report.dart';
+import 'models/function_report.dart';
+import 'models/issue.dart';
+import 'models/scoped_class_declaration.dart';
+import 'models/scoped_function_declaration.dart';
 
 abstract class ReportsBuilder {
   void recordClass(ScopedClassDeclaration declaration, ClassReport report);
 
   void recordAntiPatternCases(Iterable<Issue> issues);
+
+  void recordFunction(
+    ScopedFunctionDeclaration declaration,
+    FunctionReport report,
+  );
 
   void recordIssues(Iterable<Issue> issues);
 }

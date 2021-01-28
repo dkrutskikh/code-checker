b88b414b94c5b6969958f3be4a41be8ede67be31
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-28 21:15:52 +0300

    introduce buildFunctionReportStub

--- test/stub_builders.dart
+++ test/stub_builders.dart
@@ -1,6 +1,8 @@
+import 'package:code_checker/metrics.dart';
 import 'package:code_checker/src/metrics/number_of_methods_metric.dart';
 import 'package:code_checker/src/metrics/weight_of_class_metric.dart';
 import 'package:code_checker/src/models/class_report.dart';
+import 'package:code_checker/src/models/function_report.dart';
 import 'package:code_checker/src/models/metric_value.dart';
 import 'package:code_checker/src/models/metric_value_level.dart';
 import 'package:source_span/source_span.dart';
@@ -25,6 +27,25 @@ ClassReport buildClassReportStub({
   ];
 
   return ClassReport(
+    location: location ?? SourceSpan(SourceLocation(0), SourceLocation(0), ''),
+    metrics: [...metrics, ...defaultMetricValues],
+  );
+}
+
+FunctionReport buildFunctionReportStub({
+  SourceSpan location,
+  Iterable<MetricValue<num>> metrics = const [],
+}) {
+  const defaultMetricValues = [
+    MetricValue<int>(
+      metricsId: MaximumNestingLevelMetric.metricId,
+      value: 0,
+      level: MetricValueLevel.none,
+      comment: '',
+    ),
+  ];
+
+  return FunctionReport(
     location: location ?? SourceSpan(SourceLocation(0), SourceLocation(0), ''),
     metrics: [...metrics, ...defaultMetricValues],
   );

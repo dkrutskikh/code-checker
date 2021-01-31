82fc2ca312da85dc0d66703500da7c7ff21b6b45
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-31 21:04:19 +0300

    introduce Report.metricsLevel

--- lib/src/models/report.dart
+++ lib/src/models/report.dart
@@ -1,7 +1,9 @@
+import 'package:collection/collection.dart';
 import 'package:meta/meta.dart';
 import 'package:source_span/source_span.dart';
 
 import 'metric_value.dart';
+import 'metric_value_level.dart';
 
 /// Represents the metrics report collected for a entity
 @immutable
@@ -15,6 +17,11 @@ class Report {
   /// Returns a certain target metric
   MetricValue<num> metric(String id) => metrics
       .firstWhere((metric) => metric.metricsId == id, orElse: () => null);
+
+  // TODO(dkrutskikh): after migrate on NullSafety migrate on iterable extensions from collection package
+  MetricValueLevel get metricsLevel => metrics.isNotEmpty
+      ? metrics.reduce((a, b) => a.level > b.level ? a : b).level
+      : MetricValueLevel.none;
 
   const Report({@required this.location, @required this.metrics});
 }

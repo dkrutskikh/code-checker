01ac9c571d33ccbc499424d036b1697805e77a29
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-02-01 16:39:58 +0300

    add const constructor to Reporter

--- lib/src/reporters/reporter.dart
+++ lib/src/reporters/reporter.dart
@@ -3,4 +3,6 @@ import '../models/file_report.dart';
 /// Abstract reporter interface. Use [Runner] to get analysis info to report
 abstract class Reporter {
   void report(Iterable<FileReport> records);
+
+  const Reporter();
 }

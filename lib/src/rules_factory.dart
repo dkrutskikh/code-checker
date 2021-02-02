48b971a1c95c97730a9392055e565c1484448850
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-02-02 11:17:55 +0300

    introduce rules factory

---
+++ lib/src/rules_factory.dart
@@ -0,0 +1,16 @@
+import 'rules/newline_before_return/newline_before_return.dart';
+import 'rules/rule.dart';
+
+final _implementedRules = <String, Rule Function(Map<String, Object>)>{
+  NewlineBeforeReturnRule.ruleId: (config) =>
+      NewlineBeforeReturnRule(config: config),
+};
+
+Iterable<Rule> get allRules =>
+    _implementedRules.keys.map((id) => _implementedRules[id]({})).toList();
+
+Iterable<Rule> rulesByConfig(Map<String, Object> config) =>
+    _implementedRules.keys
+        .where((id) => config.keys.contains(id))
+        .map((id) => _implementedRules[id](config[id] as Map<String, Object>))
+        .toList();

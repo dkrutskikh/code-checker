0c44517515d67d66ae172a561ec2527fd451ee89
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-02-04 06:41:42 +0300

    rename file

--- lib/src/analyzer_plugin/plugin_utils.dart
+++ lib/src/analyzer_plugin/plugin_utils.dart
@@ -1,4 +1,5 @@
 import 'package:analyzer_plugin/protocol/protocol_common.dart' as p;
+
 import '../models/metric_value_level.dart';
 
 p.AnalysisErrorSeverity severityFromMetricValueLevel(MetricValueLevel level) =>
--- test/analyzer_plugin/plugin_utils_test.dart
+++ test/analyzer_plugin/plugin_utils_test.dart
@@ -1,6 +1,6 @@
 @TestOn('vm')
 import 'package:analyzer_plugin/protocol/protocol_common.dart';
-import 'package:code_checker/src/analyzer_plugin/analyzer_plugin_utils.dart';
+import 'package:code_checker/src/analyzer_plugin/plugin_utils.dart';
 import 'package:code_checker/src/models/metric_value_level.dart';
 import 'package:test/test.dart';
 

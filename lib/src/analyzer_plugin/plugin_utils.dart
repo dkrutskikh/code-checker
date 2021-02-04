a68da52c7040d051049ef256b8dbc02ec4c0d946
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-02-04 06:55:34 +0300

    introduce plugin util readAnalysisOptions

--- lib/src/analyzer_plugin/plugin_utils.dart
+++ lib/src/analyzer_plugin/plugin_utils.dart
@@ -1,6 +1,28 @@
+// ignore: implementation_imports
+import 'package:analyzer/src/analysis_options/analysis_options_provider.dart';
+
+// ignore: implementation_imports
+import 'package:analyzer/src/dart/analysis/driver.dart' as analyzer_internal;
 import 'package:analyzer_plugin/protocol/protocol_common.dart' as p;
 
+import '../config/analysis_options.dart';
 import '../models/metric_value_level.dart';
+import '../utils/yaml_utils.dart';
+
+AnalysisOptions readAnalysisOptions(analyzer_internal.AnalysisDriver driver) {
+  if (driver?.contextRoot?.optionsFilePath?.isNotEmpty ?? false) {
+    final file =
+        driver?.resourceProvider?.getFile(driver.contextRoot.optionsFilePath);
+    if (file?.exists ?? false) {
+      final yaml = AnalysisOptionsProvider(driver.sourceFactory)
+          .getOptionsFromFile(file);
+
+      return AnalysisOptions(yamlMapToDartMap(yaml));
+    }
+  }
+
+  return null;
+}
 
 p.AnalysisErrorSeverity severityFromMetricValueLevel(MetricValueLevel level) =>
     level == MetricValueLevel.alarm
--- test/analyzer_plugin/plugin_utils_test.dart
+++ test/analyzer_plugin/plugin_utils_test.dart
@@ -1,22 +1,71 @@
 @TestOn('vm')
+import 'package:analyzer/file_system/file_system.dart';
+import 'package:analyzer/src/context/context_root.dart' as analyzer_internal;
+import 'package:analyzer/src/dart/analysis/driver.dart' as analyzer_internal;
 import 'package:analyzer_plugin/protocol/protocol_common.dart';
 import 'package:code_checker/src/analyzer_plugin/plugin_utils.dart';
 import 'package:code_checker/src/models/metric_value_level.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+class AnalysisDriverMock extends Mock
+    implements analyzer_internal.AnalysisDriver {}
+
+// ignore: avoid_implementing_value_types
+class ContextRootMock extends Mock implements analyzer_internal.ContextRoot {}
+
+class ResourceProviderMock extends Mock implements ResourceProvider {}
+
+class FileMock extends Mock implements File {}
+
 void main() {
-  test(
-    'severityFromMetricValueLevel converts MetricValueLevel to AnalysisErrorSeverity',
-    () {
-      expect(
-        MetricValueLevel.values.map(severityFromMetricValueLevel),
-        equals([
-          AnalysisErrorSeverity.INFO,
-          AnalysisErrorSeverity.INFO,
-          AnalysisErrorSeverity.INFO,
-          AnalysisErrorSeverity.WARNING,
-        ]),
-      );
-    },
-  );
+  group('analyzer plugin utils', () {
+    test(
+      'readAnalysisOptions constructs AnalysisOptions from driver context',
+      () {
+        const analysisOptionsPath = 'analysis_options.yaml';
+
+        expect(readAnalysisOptions(null), isNull);
+
+        final driver = AnalysisDriverMock();
+        expect(readAnalysisOptions(driver), isNull);
+
+        final contextRoot = ContextRootMock();
+        when(driver.contextRoot).thenReturn(contextRoot);
+        expect(readAnalysisOptions(driver), isNull);
+
+        when(contextRoot.optionsFilePath).thenReturn('');
+        expect(readAnalysisOptions(driver), isNull);
+
+        when(contextRoot.optionsFilePath).thenReturn(analysisOptionsPath);
+        expect(readAnalysisOptions(driver), isNull);
+
+        final resourceProvider = ResourceProviderMock();
+        when(driver.resourceProvider).thenReturn(resourceProvider);
+        expect(readAnalysisOptions(driver), isNull);
+
+        final file = FileMock();
+        when(resourceProvider.getFile(analysisOptionsPath)).thenReturn(file);
+        expect(readAnalysisOptions(driver), isNull);
+
+        when(file.exists).thenReturn(true);
+        expect(readAnalysisOptions(driver), isNotNull);
+      },
+    );
+
+    test(
+      'severityFromMetricValueLevel converts MetricValueLevel to AnalysisErrorSeverity',
+      () {
+        expect(
+          MetricValueLevel.values.map(severityFromMetricValueLevel),
+          equals([
+            AnalysisErrorSeverity.INFO,
+            AnalysisErrorSeverity.INFO,
+            AnalysisErrorSeverity.INFO,
+            AnalysisErrorSeverity.WARNING,
+          ]),
+        );
+      },
+    );
+  });
 }

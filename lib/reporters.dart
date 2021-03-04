30405422a1ccb1704f79908171dac0b329124a1b
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-03-04 06:33:26 +0300

    cleanup reexports

--- lib/reporters.dart
+++ lib/reporters.dart
@@ -1,3 +1,1 @@
 export 'package:code_checker/src/reporter_factory.dart';
-export 'package:code_checker/src/reporters/console_reporter.dart';
-export 'package:code_checker/src/reporters/json_reporter.dart';
